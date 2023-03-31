7cbe36adaff27c600df8f0d58c50e5cbae734900
Rutvik_patel <rutvikpatel7534@gmail.com>
2023-03-31 17:05:54 +0530

    Changes at 31-03-23

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
-function App() {
-  return (
-    <>
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar />
+      <News />
     </>
-  );
+    )
+  }
 }
-
-export default App;

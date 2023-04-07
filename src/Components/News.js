22654539d1d0561f953f94850a2f5dc3d9154b8f
Rutvik_patel <rutvikpatel7534@gmail.com>
2023-04-07 12:24:15 +0530

    '07-04-23'

--- src/Components/News.js
+++ src/Components/News.js
@@ -17,33 +17,34 @@ export class News extends Component {
         let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=1&pageSize=20";
         let data = await fetch(url);
         let parseData = await data.json();
-        this.setState({ articales: parseData.articles,totalResults:parseData.totalResults });
+        this.setState({ articales: parseData.articles, totalResults: parseData.totalResults });
 
 
     }
 
-     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page-1}&pageSize=20`;
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page - 1}&pageSize=20`;
         let data = await fetch(url);
         let parseData = await data.json();
         this.setState({
-            page : this.state.page-1,
+            page: this.state.page - 1,
             articales: parseData.articles
         })
     }
 
-     handleNextClick = async () => {
-        if(this.state.page+1 >Math.ceil(this.state.totalResults/20)){
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
 
         }
-        else{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page+1}&pageSize=20`;
-        let data = await fetch(url);
-        let parseData = await data.json();
-        this.setState({
-            page : this.state.page+1,
-            articales: parseData.articles
-        })}
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parseData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articales: parseData.articles
+            })
+        }
     }
 
 
@@ -63,10 +64,9 @@ export class News extends Component {
                     })}
                 </div>
                 <div className="container d-flex justify-content-between">
-                <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-                <button type="button"  className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
-                
             </div>
         )
     }

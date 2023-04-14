8cbcb102b629634c211e34ff486ea4eeb1592cbb
Rutvik_patel <rutvikpatel7534@gmail.com>
2023-04-14 16:26:50 +0530

    "Changes"

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -14,35 +15,41 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=1&pageSize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=1&pageSize=$${this.props.pageSize}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parseData = await data.json();
-        this.setState({ articales: parseData.articles, totalResults: parseData.totalResults });
+        this.setState({ articales: parseData.articles, 
+            totalResults: parseData.totalResults,
+            loading: false 
+        });
 
 
     }
 
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page - 1}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parseData = await data.json();
         this.setState({
             page: this.state.page - 1,
-            articales: parseData.articles
+            articales: parseData.articles,
+            loading:false
         })
     }
 
     handleNextClick = async () => {
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-
-        }
-        else {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page + 1}&pageSize=20`;
+        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / 20))) {
+       
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5be166f7c10b438fa652ce9af6528651&page=${this.state.page + 1}&pageSize=$${this.props.pageSize}`;
+            this.setState({loading: true});
             let data = await fetch(url);
             let parseData = await data.json();
             this.setState({
                 page: this.state.page + 1,
-                articales: parseData.articles
+                articales: parseData.articles,
+                loading: false
             })
         }
     }
@@ -52,12 +59,13 @@ export class News extends Component {
         return (
             <div className='container my-3' style={{ alignItems: 'center' }}>
                 <h1 className="text-center">News Top Headlines</h1>
+                {this.state.loading &&<Spinner/>}
                 <div className="row">
-                    {this.state.articales.map((element) => {
-                        return <div className="col-md-3" key={element.url}>
+                    {!this.state.loading && this.state.articales.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
                             <NewsItem
-                                title={element.title ? element.title.slice(0, 35) : ''}
-                                description={element.description ? element.description.slice(0, 60) : ''}
+                                title={element.title ? element.title.slice(0, 50) : ''}
+                                description={element.description ? element.description.slice(0, 120) : ''}
                                 imageURL={element.urlToImage}
                                 newsURL={element.url} />
                         </div>
@@ -65,7 +73,7 @@ export class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -11,6 +11,7 @@ export class NewsItem extends Component {
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
                         <a href={newsURL} target='_blank' className="btn btn-sm btn-dark" rel="noreferrer" >Read more</a>
+                       
                     </div>
                 </div>
             </div>
